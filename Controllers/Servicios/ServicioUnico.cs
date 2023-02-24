a8d7c740399fdfe38fa28a296b6ee8c84d0f7efe
unknown <zerpased@gmail.com>
2023-02-24 12:23:01 -0400

    portafolio

---
+++ Controllers/Servicios/ServicioUnico.cs
@@ -0,0 +1,33 @@
+namespace Portafolio.Servicios
+{
+    public class ServicioUnico
+    {
+        public ServicioUnico()
+        {
+            ObtenerGuid = Guid.NewGuid();
+        }
+
+        public Guid ObtenerGuid { get; private set; }
+    }
+
+    public class ServicioDelimitado
+    {
+        public ServicioDelimitado()
+        {
+            ObtenerGuid = Guid.NewGuid();
+        }
+
+        public Guid ObtenerGuid { get; private set; }
+    }
+
+    public class ServicioTransitorio()
+    
+    {
+        public ServicioTransitorio()
+        {
+            ObtenerGuid = Guid.NewGuid();
+        }
+
+        public Guid ObtenerGuid { get; private set; }
+    }
+}
